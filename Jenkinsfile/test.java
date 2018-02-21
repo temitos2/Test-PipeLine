7f65404d0ed4eccba5561a6c33b5448f7ca9f78e
root <root@ip-172-16-1-132.ec2.internal>
2018-02-21 00:54:30 +0000

    still testing

--- Jenkinsfile/test.java
+++ Jenkinsfile/test.java
@@ -1,19 +1,30 @@
-
-Jenkinsfile (Declarative Pipeline)
-
-pipeline {
-    agent any
-    stages {
-        stage('Build') {
-            steps {
-                sh 'echo "Hello World"'
-                sh '''
-                    echo "Multiline shell steps works too"
-                    ls -lah
-                '''
-            }
-        }
+{
+  "Resources" : {
+    "DB" : {
+      "Type" : "AWS::RDS::DBInstance",
+      "Properties" : {
+        "AllocatedStorage" : "5",
+        "StorageType" : "gp2",
+        "DBInstanceClass" : "db.t2.micro",
+        "DBName" : "wordpress",
+        "Engine" : "MySQL",
+        "MasterUsername" : "wordpress",
+        "MasterUserPassword" : "w0rdpr355"
+      }
+    },
+    "EC2" : {
+      "Type" : "AWS::EC2::Instance",
+      "Properties" : {
+        "ImageId" : "ami-c481fad3",
+        "InstanceType" : "t2.micro"
+      }
+    },
+    "S3" : {
+      "Type" : "AWS::S3::Bucket",
+      "Properties" : {
+        "BucketName" : "wp-xxxxxx"
+      }
     }
+  }
 }
-
 
